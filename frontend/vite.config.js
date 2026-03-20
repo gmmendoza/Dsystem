1a29adf87a80c2e9439c35eab9b271e05a21a46b
SGT <guuamendoza@gmail.com>
2026-03-20 10:50:23 -0300

    feat: setup automated github actions deployment and 404 router fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DSystem/', // Reemplaza con el nombre exacto de tu repositorio en GitHub
 })
